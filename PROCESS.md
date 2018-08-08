3dbd173e6bbc05ba8a4a3bbad0acfaf672dcd159
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2018-08-08 22:38:49 +0100

    Added comments to guide and components

--- PROCESS.md
+++ PROCESS.md
@@ -7,13 +7,20 @@ Process so far
 4. Relise I can't open the .skech file because of lack of access to a mac, so get an account on figma and avocode to view the file in Windows and Linux (bit of juggling there)
 5. Extract css and start developing basic presentational components, using storybook for instant feed back
 6. Start to place the components into the main `App.tsx` file to place the componets in the right place.
-
+7. Keep developing presitional components to better match the wireframe.
+8. Create the Converter stateful component to deal with the currency conversion between sending and reciving, including fees
+9. Implement CSS Grid for the main page layout (header, main column, secondary column)
+10. Implement `redux` and `react-redux`. Redux will be used to manage the display state of the modal, and sync the Converter and Details componets.
+11. Set up the Modal and Identity Verification componets
+12. Finalize mobile responsiveness
+13. Clean up, finish writing notes
+14. Submit project.
 
 
 Problems faced so far:
-- Skecth file game me some issues due to not having a mac, attemped to import into Avocode, but had to do it through Figma, because the file was made with an older version of Sketch.
+- Sketch file game me some issues due to not having a mac, attemped to import into Avocode, but had to do it through Figma, because the file was made with an older version of Sketch.
 - I had some issues with the fonts (Post Grotesk) importing into `create-react-app` and Avocode, had to do some trial and error before finding the right solution
-- Had to do some heavy editing of the Webpack file for storybook due to it not loading the svg logo proberly when `create-react-app` didn't have issues.
-  - I'm using an alpha version of `@storybook/react` which has updated its default svg loader to something a bit more unreiliable.
-- Was a bit of trouble getting the boxes of the converters to have proper border colours and radius. There not acually all in the same box; there a seris of boxes with diffreent styles depending on there position in relation to there siblings and whether or not they are active and have to show a blue border, without having a double space border.
+- Had to do some heavy editing of the Webpack file for storybook due to it not loading the svg logo probably when `create-react-app` didn't have issues.
+  - I'm using an alpha version of `@storybook/react` which has updated its default svg loader to something a bit more unreliable.
+- Was a bit of trouble getting the boxes of the converters to have proper border colours and radius. There not actually all in the same box; there a serice of boxes with diffrent styles depending on there position in relation to there siblings and whether or not they are active and have to show a blue border, without having a double space border.
 - Lot of work had to be done with the CSS grid on the main page, especally since I was nesting grids inside sub sections of the page. I put all of the grid configuration in it's own page to cut down on the length of `App.tsx`
--- src/components/Converter/View.tsx
+++ src/components/Converter/View.tsx
@@ -33,49 +33,93 @@ const convertCurrency = (money: CurrencyInput, rate: number) =>
 const currencyLessThen = (a: CurrencyInput, b: CurrencyInput) =>
   currency(a).value <= currency(b).value;
 
+/**
+ * Displays two fields to convert between euro and pounds
+ *
+ */
 class View extends Component<IConverterProps, IState> {
+  /**
+   * React state
+   * Contains the value of the euro and pound inputs
+   */
   public state = {
     eurValue: currency(0),
     gbpValue: currency(0)
   };
 
+  /**
+   * Creates the componenet
+   * @param props Contains default values as well as the converison rate and fee
+   */
   constructor(props: IConverterProps) {
     super(props);
+
+    // If the default currency is in euros
     if (props.defaultCurrency === 'EUR') {
       const defaultMoney = currency(this.props.defaultValue);
-      if (defaultMoney.value === 0 || currencyLessThen(defaultMoney.value, this.props.fee)) {
+
+      // If the value is 0 OR the result is less then the fee
+      if (
+        defaultMoney.value === 0 ||
+        currencyLessThen(
+          convertCurrency(defaultMoney, this.props.eurToGbpRate),
+          this.props.fee
+        )
+      ) {
+        // The result of the converison will be completly eating by the fee,
+        // Leaving the reciver with nothing
+        // Probaly would be an error message in a production app
         this.state = {
           eurValue: defaultMoney,
-          gbpValue: currency(0),
+          gbpValue: currency(0)
         };
       } else {
+        // Convert the currency to pounds
+        // Then subtract the fee
         this.state = {
           eurValue: defaultMoney,
-          gbpValue: convertCurrency(defaultMoney, this.props.eurToGbpRate).subtract(
-            this.props.fee
-          )
-        }
-      };
+          gbpValue: convertCurrency(
+            defaultMoney,
+            this.props.eurToGbpRate
+          ).subtract(this.props.fee)
+        };
+      }
+
+      // Return the componet, do not go to next if statement
       return this;
     }
 
+    // If the default currency is in pounds
     if (props.defaultCurrency === 'GBP') {
       const defaultMoney = currency(this.props.defaultValue);
+      // If the value is 0
       if (defaultMoney.value === 0) {
         this.state = {
           eurValue: currency(0),
-          gbpValue: currency(0),
+          gbpValue: currency(0)
         };
       } else {
+        // Add the fee back to the pound value
+        // (Remember, this is the reverse of the euro operation)
+        // Then convert to euro
         this.state = {
-          eurValue: convertCurrency(defaultMoney.add(this.props.fee), 1 / this.props.eurToGbpRate),
+          eurValue: convertCurrency(
+            defaultMoney.add(this.props.fee),
+            1 / this.props.eurToGbpRate
+          ),
           gbpValue: defaultMoney
-        }
-      };
+        };
+      }
       return this;
     }
   }
 
+  /**
+   * Update any listners
+   * Used to keep the Redux dispatcher up to date
+   * @param prevProps last version of props
+   * @param prevState last version of state
+   */
   public componentDidUpdate(prevProps: IConverterProps, prevState: IState) {
     if (
       (this.state.eurValue !== prevState.eurValue ||
@@ -86,6 +130,9 @@ class View extends Component<IConverterProps, IState> {
     }
   }
 
+  /**
+   * React render method
+   */
   public render() {
     return (
       <div className={cx(this.props.className, converterContainerClassName)}>
@@ -97,12 +144,18 @@ class View extends Component<IConverterProps, IState> {
     );
   }
 
+  // Dynamic onChange handlers
+  // See construtor for details on logic
+
   private updateEurValue = (newValue: string) => {
     const newMoney = currency(newValue);
-    if (newMoney.value === 0 || currencyLessThen(newMoney.value, this.props.fee)) {
+    if (
+      newMoney.value === 0 ||
+      currencyLessThen(newMoney.value, this.props.fee)
+    ) {
       this.setState({
         eurValue: newMoney,
-        gbpValue: currency(0),
+        gbpValue: currency(0)
       });
     } else {
       this.setState({
@@ -111,7 +164,7 @@ class View extends Component<IConverterProps, IState> {
           this.props.fee
         )
       });
-    };
+    }
   };
 
   private updateGbpValue = (newValue: string) => {
@@ -119,41 +172,51 @@ class View extends Component<IConverterProps, IState> {
     if (newMoney.value === 0) {
       this.setState({
         eurValue: currency(0),
-        gbpValue: currency(0),
+        gbpValue: currency(0)
       });
     } else {
       this.setState({
-        eurValue: convertCurrency(newMoney.add(this.props.fee), 1 / this.props.eurToGbpRate),
-        gbpValue: newMoney,
+        eurValue: convertCurrency(
+          newMoney.add(this.props.fee),
+          1 / this.props.eurToGbpRate
+        ),
+        gbpValue: newMoney
       });
     }
   };
 
+
+  /**
+   * Render prop for ActiveFieldTracker
+   * The purpose of ActiveFieldTracker is to track the currently selected field,
+   * which will be highlighted in blue.
+   * Otherwise these are normal React controlled form components
+   */
   private renderFields: IActiveFieldTrackerRenderProp = (
     isActive,
     makeActive
   ) => (
-      <>
-        <FieldChild
-          key={fieldKeys.EUR}
-          value={this.state.eurValue.format(false)}
-          updateValue={this.updateEurValue}
-          currency="EUR"
-          active={isActive(fieldKeys.EUR)}
-          onClick={makeActive(fieldKeys.EUR)}
-          description="you send"
-        />
-        <FieldChild
-          key={fieldKeys.GBP}
-          updateValue={this.updateGbpValue}
-          value={this.state.gbpValue.format(false)}
-          currency="GBP"
-          active={isActive(fieldKeys.GBP)}
-          onClick={makeActive(fieldKeys.GBP)}
-          description="reciver gets"
-        />
-      </>
-    );
+    <>
+      <FieldChild
+        key={fieldKeys.EUR}
+        value={this.state.eurValue.format(false)}
+        updateValue={this.updateEurValue}
+        currency="EUR"
+        active={isActive(fieldKeys.EUR)}
+        onClick={makeActive(fieldKeys.EUR)}
+        description="you send"
+      />
+      <FieldChild
+        key={fieldKeys.GBP}
+        updateValue={this.updateGbpValue}
+        value={this.state.gbpValue.format(false)}
+        currency="GBP"
+        active={isActive(fieldKeys.GBP)}
+        onClick={makeActive(fieldKeys.GBP)}
+        description="reciver gets"
+      />
+    </>
+  );
 }
 
 export default View;
